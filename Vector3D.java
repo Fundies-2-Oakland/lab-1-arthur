740248a5147c24e35643db92ac024d11ede7599a
Arthur Raoult <art.raoult@gmail.com>
2025-01-10 11:13:30 -0800

    toString & Mag

--- Vector3D.java
+++ Vector3D.java
@@ -1,4 +1,6 @@
+import java.math.*;
 
+import static java.lang.Math.acos;
 
 
 public class Vector3D {
@@ -28,6 +30,16 @@ public class Vector3D {
         return z;
     }
 
+    public double getMagnitude() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+
+    // Override toString
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
 
 
 }
