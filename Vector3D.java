be5026252d1beda39bce65de853d100aee97dd7e
Arthur Raoult <art.raoult@gmail.com>
2025-01-10 11:02:35 -0800

    Vector 3D get Methods

---
+++ Vector3D.java
@@ -0,0 +1,33 @@
+
+
+
+public class Vector3D {
+
+    //
+    private double x;
+    private double y;
+    private double z;
+
+    // Constructor
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    // Getter
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+
+
+}
